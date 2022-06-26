1b6ed7ef3123106a62e2ad84029b691e7d722ef7
khatriamit <rajukhatri054@gmail.com>
2022-06-26 10:04:41 +0545

    create token transfer function

--- programs/token-contract/src/lib.rs
+++ programs/token-contract/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token;
-use anchor_spl::token::MintTo;
+use anchor_spl::token::{MintTo, Token, Transfer};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -22,6 +22,19 @@ pub mod token_contract {
         token::mint_to(cpi_ctx, 10)?;
         Ok(())
     }
+
+    pub fn transfer_token(ctx: Context<TransferToken>) -> Result<()> {
+        let transfer_instruction = Transfer {
+            from: ctx.accounts.from.to_account_info(),
+            to: ctx.accounts.to.to_account_info(),
+            authority: ctx.accounts.signer.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+
+        let cpi_ctx = CpiContext::new(cpi_program, transfer_instruction);
+        anchor_spl::token::transfer(cpi_ctx, 5)?;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -33,4 +46,15 @@ pub struct MintToken<'info> {
     pub token_account: UncheckedAccount<'info>,
     #[account(mut)]
     pub payer: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct TransferToken<'info> {
+    pub token_program: Program<'info, Token>,
+    #[account(mut)]
+    pub from: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub to: AccountInfo<'info>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
 }
