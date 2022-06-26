0259fcec10ce7595cb80c66e391adb45932a7387
khatriamit <rajukhatri054@gmail.com>
2022-06-26 09:44:21 +0545

    write function to mint token

--- programs/token-contract/src/lib.rs
+++ programs/token-contract/src/lib.rs
@@ -1,15 +1,36 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token;
+use anchor_spl::token::MintTo;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod token_contract {
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn mint_token(ctx: Context<MintToken>) -> Result<()> {
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.payer.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        token::mint_to(cpi_ctx, 10)?;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct MintToken<'info> {
+    #[account(mut)]
+    pub mint: UncheckedAccount<'info>,
+    pub token_program: Program<'info, Token>,
+    #[account(mut)]
+    pub token_account: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub payer: AccountInfo<'info>,
+}
